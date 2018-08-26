0a6e67091ea5107a29a00ecf8b2621293c65b947
Anwana Ntofon <anwanantofon@gmail.com>
2018-08-25 17:53:32 -0700

    First blank card clicked is now defined and tracking the type of icon selected. Also some consolidation of function and code cleanup

--- js/script.js
+++ js/script.js
@@ -15,8 +15,6 @@ $(document).ready(function() {
   const cardStyle = $(".card-style");
   const animateCard = $(".animate-card");
   const blankCard = $(".blank-card");
-  // const absolute = $(".absolute");
-  // const flipContainer = $(".flip-container");
 
   // Initial States
 
@@ -31,12 +29,26 @@ $(document).ready(function() {
   let timer = 0;
 
   // Created list that holds all of the cards
-  let cardArray = ['fa-angular', 'fa-css3-alt', 'fa-vuejs', 'fa-react', 'fa-sass', 'fa-js-square', 'fa-angular', 'fa-html5', 'fa-node-js', 'fa-react', 'fa-css3-alt', 'fa-node-js', 'fa-vuejs', 'fa-html5', 'fa-js-square', 'fa-sass'];
-
+  let cardArray = [
+    'fa-angular',
+    'fa-css3-alt',
+    'fa-vuejs',
+    'fa-react',
+    'fa-sass',
+    'fa-js-square',
+    'fa-angular',
+    'fa-html5',
+    'fa-node-js',
+    'fa-react',
+    'fa-css3-alt',
+    'fa-node-js',
+    'fa-vuejs',
+    'fa-html5',
+    'fa-js-square',
+    'fa-sass'
+  ];
 
   // NOTE: REMOVE CONSOLE.LOG STATEMENTS USED FOR DEBUGGING PURPOSES AFTER PROJECT COMPLETION
-
-
 
   /*
    * Display the cards on the page
@@ -45,99 +57,75 @@ $(document).ready(function() {
    *   - add each card's HTML to the page
    */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  // Shuffle function from http://stackoverflow.com/a/2450976
+  function shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
 
     while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
     }
 
     return array;
-} // shuffle()
+  } // shuffle()
 
+  /* set up the event listener for a card. If a card is clicked:  - display the card's symbol (put this functionality in another function that you call from this one)  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)  - if the list already has another card, check to see if the two cards match    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one) */
 
+  function restartGame() {
+    clearInterval(timer);
+    initialGameState();
+    // Set vars back to initial state logically
+    seconds = 1;
+    minutes = 0;
+    tmp = 0;
+    movesCount = 0;
+    // starCount = 3;
+    compareMatches.length = 0;
+    correctMatches.length = 0;
+    score = 0;
+    timer = 0;
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+    // Set initial state of HTML
 
- function restartGame() {
-   clearInterval(timer);
-   initialGameState();
-   // Set vars back to initial state logically
-   seconds = 1;
-   minutes = 0;
-   tmp = 0;
-   movesCount = 0;
-   // starCount = 3;
-   compareMatches.length = 0;
-   correctMatches.length = 0;
-   score = 0;
-   timer = 0;
+    minutesField.text('00');
+    secondsField.text('00');
+    movesField.text('00');
 
-   // Set initial state of HTML
+    // Call function to reshuffle cards and add the HTML to the screen
 
-   minutesField.text('00');
-   secondsField.text('00');
-   movesField.text('00');
+    displayCard(cardArray);
 
-   // Call function to reshuffle cards and add the HTML to the screen
+    // modify classes back to full stars, set count to 3
 
-   displayCard(cardArray);
+    switch (starCount) {
+      case 1:
+        faStar1.removeClass('far');
+        faStar1.addClass('fas');
+        faStar2.removeClass('far');
+        faStar2.addClass('fas');
+        starCount = 3;
+        break;
 
-   // modify classes back to full stars, set count to 3
+      case 2:
+        faStar1.removeClass('far');
+        faStar1.addClass('fas');
+        starCount = 3;
+        break;
 
-   switch (starCount) {
-     case 1:
-       faStar1.removeClass('far');
-       faStar1.addClass('fas');
-       faStar2.removeClass('far');
-       faStar2.addClass('fas');
-       starCount = 3;
-       break;
-
-     case 2:
-       faStar1.removeClass('far');
-       faStar1.addClass('fas');
-       starCount = 3;
-       break;
-
-     default:
-       starCount = 3;
-       break;
-   }
- } // restartGame()
+      default:
+        starCount = 3;
+        break;
+    }
+  } // restartGame()
 
   function endGame() {
     alert("Time's up, please press the 'Restart' button or refresh the page");
   } // endGame()
-
-  // function reset () {
-  //   // console.log("Cards do not match.");
-  //   absolute.css("z-index", -1);
-  //   flipContainer.removeClass('animated flipInY');
-  //   absolute.addClass('animated flipOutY');
-  //   absolute.removeClass('animated flipOutY');
-  //   $(".front").html("Front");
-  // }
-
-  function flipAnimation() {
-    // $('body').css({"background" : "purple", "transition": ".2s all ease-in-out"});
-
-  } // flipAnimation()
-
 
   function completeGame() {
     //The below if / else if statements are for formatting purposes when the user wins the game
@@ -152,17 +140,15 @@ function shuffle(array) {
           <div class="btn btn-primary" id="user-refresh">Refresh</div>
         </div>
       </div>`);
-      const playAgain = $('#play-again');
-      const userRefresh = $('#user-refresh');
-      playAgain.click(restartGame);
-      userRefresh.click(function(){
-        return location.reload();
-      });
+        const playAgain = $('#play-again');
+        const userRefresh = $('#user-refresh');
+        playAgain.click(restartGame);
+        userRefresh.click(function() {
+          return location.reload();
+        });
         // alert(`Congratulations, you've won the game!\nTime (minutes and seconds): 0${minutes}:0${seconds}.\nStar rating: ${starCount}`);
         // restartGame();
-      }
-
-      else {
+      } else {
         // $("body").html("");
         // empty html, display congrtulations modal
         $('body').append(`<div class="card" style="width: 250px; height: 250px;">
@@ -172,18 +158,16 @@ function shuffle(array) {
           <div class="btn btn-primary" id="user-refresh">Refresh</div>
         </div>
       </div>`);
-      const playAgain = $('#play-again');
-      const userRefresh = $('#user-refresh');
-      playAgain.click(restartGame);
-      userRefresh.click(function(){
-        return location.reload();
-      });
+        const playAgain = $('#play-again');
+        const userRefresh = $('#user-refresh');
+        playAgain.click(restartGame);
+        userRefresh.click(function() {
+          return location.reload();
+        });
         // alert(`Congratulations, you've won the game!\nTime (minutes and seconds): 0${minutes}:${seconds}.\nStar rating: ${starCount}`);
         // restartGame();
       }
-    }
-
-    else if (score === 8 && seconds <= 9) {
+    } else if (score === 8 && seconds <= 9) {
       if (minutes <= 9) {
         // $("body").html("");
         // empty html, display congrtulations modal
@@ -194,17 +178,15 @@ function shuffle(array) {
           <div class="btn btn-primary" id="user-refresh">Refresh</div>
         </div>
       </div>`);
-      const playAgain = $('#play-again');
-      const userRefresh = $('#user-refresh');
-      playAgain.click(restartGame);
-      userRefresh.click(function(){
-        return location.reload();
-      });
+        const playAgain = $('#play-again');
+        const userRefresh = $('#user-refresh');
+        playAgain.click(restartGame);
+        userRefresh.click(function() {
+          return location.reload();
+        });
         // alert(`Congratulations, you've won the game!\nTime (minutes and seconds): 0${minutes}:0${seconds}.\nStar rating: ${starCount}`);
         // restartGame();
-      }
-
-      else {
+      } else {
         // $("body").html("");
         // empty html, display congrtulations modal
         $('body').append(`<div class="card" style="width: 250px; height: 250px;">
@@ -214,40 +196,35 @@ function shuffle(array) {
           <div class="btn btn-primary" id="user-refresh">Refresh</div>
         </div>
       </div>`);
-      const playAgain = $('#play-again');
-      const userRefresh = $('#user-refresh');
-      playAgain.click(restartGame);
-      userRefresh.click(function(){
-        return location.reload();
-      });
+        const playAgain = $('#play-again');
+        const userRefresh = $('#user-refresh');
+        playAgain.click(restartGame);
+        userRefresh.click(function() {
+          return location.reload();
+        });
         // alert(`Congratulations, you've won the game!\nTime (minutes and seconds): ${minutes}:0${seconds}.\nStar rating: ${starCount}`);
         // restartGame();
       }
     }
   } // completeGame()
 
-
   function checkMatch(event) {
+    console.log(event);
     //store class name of elements in an array to be compared for match/no match
-    // absolute.css("z-index", 1);
-    // flipContainer.addClass('animated flipInY');
-    compareMatches.push(event.target.classList[4]);
 
-    //at 850 milliseconds, call the flipAnimation function
-
-    // setTimeout(flipAnimation, 850);
+    compareMatches.push(event);
 
     console.log(compareMatches);
-    if(compareMatches.length === 2) {
+    if (compareMatches.length === 2) {
       movesCount += 1;
       // 1 move = user click of 2 cards (pair)
-      if(compareMatches[0] === compareMatches[1]) {
-        if($.inArray(compareMatches[0], correctMatches) !== -1 || $.inArray(compareMatches[1], correctMatches) !== -1) {
+      if (compareMatches[0] === compareMatches[1]) {
+        if ($.inArray(compareMatches[0], correctMatches) !== -1 || $.inArray(compareMatches[1], correctMatches) !== -1) {
           console.log("Card has already been used");
-          compareMatches.length = 0;
-        } // if the cards being compared are already a matched pair, empty array to check a new pair of cards
+          compareMatches.length = 0; // if the cards being compared are already a matched pair, empty array to check a new pair of cards
 
-        else {
+
+        } else {
           correctMatches.push(compareMatches[0], compareMatches[1]);
           console.log(`Card: ${compareMatches[0]}\nCard: ${compareMatches[1]}.\nCards match.`);
           console.log(`Correct matches: ${correctMatches}`);
@@ -260,23 +237,16 @@ function shuffle(array) {
 
         // call completeGame function once user wins game
 
-        setTimeout(completeGame, 1000);
+        setTimeout(completeGame, 1000); // if statement for match
 
-      } // if statement for match
-
-      else {
+      } else {
         console.log(`Score: ${score}`);
         console.log(`Card: ${compareMatches[0]}\nCard: ${compareMatches[1]}.\nCards do not match.`);
         compareMatches.length = 0;
-        // setTimeout(reset, 2000);
-        // absolute.removeClass('animated flipOutY');
-        // resetFlip();
-
         //delete contents instead of assigning reference to a new array (other references untouched) and causing potential memory leaks: https://www.jstips.co/en/javascript/two-ways-to-empty-an-array/
       } // else statement for no match
     } // user selects two cards to be compared for a match
   } //checkMatch()
-
 
   function displayCard(cardArray) {
     // reshuffle cards before display to the screen
@@ -289,12 +259,14 @@ function shuffle(array) {
 
     // add HTML for the card icon to the screen with a corresponding icon class
 
-    for(let i = 0; i < cardArray.length; i++) {
+    for (let i = 0; i < cardArray.length; i++) {
+      // $(this).remove();
       console.log(`Index: ${i}, Class: ${cardArray[i]}`);
-      $('.card-style:eq('+i+')').append(`<span class="fab fa-4x align-icon icon" id="icon-${i}"></span>`);
-      $('.icon:eq('+i+')').addClass(cardArray
-      [i]);
+      $('.card-style:eq(' + i + ')').append(`<span class="fab fa-4x align-icon icon" id="icon-${i}"></span>`);
+      $('.icon:eq(' + i + ')').addClass(cardArray[i]);
 
+      // $(".fab").css("display", "block");
+      // animateCard.addClass("animated flipInY slow delay-4s");
 
     }
 
@@ -314,32 +286,28 @@ function shuffle(array) {
     }
 
     switch (movesCount) {
-      //two clicks = 1 try. Both cases are being ran twice because (24 to case 25 = decrement starCount, case 25 to 26 = clicked again to flip) starCount initially decrements twice in both cases. Current solution only decrements the count once in each case, even though case 25 goes through two passes to get to 26
+        //two clicks = 1 try. Both cases are being ran twice because (24 to case 25 = decrement starCount, case 25 to 26 = clicked again to flip) starCount initially decrements twice in both cases. Current solution only decrements the count once in each case, even though case 25 goes through two passes to get to 26
       case 25:
-      if (starCount === 2) {
-        // do nothing, already decremented from 3 to 2 stars
-      }
+        if (starCount === 2) {
+          // do nothing, already decremented from 3 to 2 stars
+        } else {
+          faStar1.removeClass('fas');
+          faStar1.addClass('far');
+          //remove and add the FontAwesome classes for a Star
+          starCount--;
+        }
 
-      else {
-        faStar1.removeClass('fas');
-        faStar1.addClass('far');
-        //remove and add the FontAwesome classes for a Star
-        starCount--;
-      }
-
-      console.log(`Stars: ${starCount}`);
-      break;
+        console.log(`Stars: ${starCount}`);
+        break;
 
       case 35:
-      if (starCount === 1) {
-        // do nothing, already decremented from 2 to 1 stars
-      }
-
-      else {
-        faStar2.removeClass('fas');
-        faStar2.addClass('far');
-        starCount--;
-      }
+        if (starCount === 1) {
+          // do nothing, already decremented from 2 to 1 stars
+        } else {
+          faStar2.removeClass('fas');
+          faStar2.addClass('far');
+          starCount--;
+        }
 
         console.log(`Stars: ${starCount}`);
         break;
@@ -349,8 +317,7 @@ function shuffle(array) {
     } // switch statement swaps the FontAwesome star class after certain amount of moves
   } // countMoves()
 
-
-  function startTime() {
+  function countTime() {
     tmp = seconds + 1;
     // tmp variable used to enter if stmt when on the verge of "60 seconds" to become "1 minute 0 seconds"
     // console.log(`Minute: ${minutes}, Seconds: ${seconds}`);
@@ -383,57 +350,51 @@ function shuffle(array) {
     }
   } // startTime()
 
+  function startTimer() {
+    timer = setInterval(countTime, 1000);
+  }
 
-  playButton.click(function() {
-    timer = setInterval(startTime, 1000);
+  function initialGameState() {
+    $(".icon").remove();
+    $("<span></span>").addClass('blank-card');
+    $(".fab").css("display", "none");
+  }
 
-  });
-
-
-
-  cardStyle.click(function() {
-    checkMatch(event);
-    //prints class that user selects ultimately logging the class name clicked,
-
+  function selectCard() {
+    //must come first to allow the $(this) statment that accesses the class to come into play
     blankCard.remove();
-      $(".fab").css("display", "block");
+    //prints class that user selects ultimately logging the class name clicked,
+    console.log(`The class is: ${$(this).children().attr('class').split(' ')[4]}`);
     $(this).addClass("animated flipInY slow delay-4s");
-console.log(`The class is: ${$(this).children().attr('class').split(' ')[4]}`);
+    checkMatch($(this).children().attr('class').split(' ')[4]);
+
     // setTimeout(function() {
     //   $(this).removeClass("animated flipInY slow delay-4s");
-    //   // $(this).addClass("animated flipOutY slow delay-4s");
+    //    $(this).addClass("animated flipOutY slow delay-4s");
     //
     // },2000)
     countMoves();
-  });
+  }
 
-  restartButton.click(restartGame);
-
-function flipCard () {
-  // actually shuffle and display cards upon flip, might want to leave it outside function to randomize right away
+  function selectBlankCard() {
+    displayCard(cardArray);
 $(".fab").css("display", "block");
-blankCard.remove();
-  animateCard.addClass("animated flipInY slow delay-4s");
-
-}
-
-function initialGameState() {
-  $(".icon").remove();
-  $("<span></span>").addClass('blank-card');
-$(".fab").css("display", "none");
-}
-
-initialGameState();
-
-blankCard.click(function() {
-  displayCard(cardArray);
-  $(".fab").css("display", "none");
-
-
   // $(".fab").css("display", "none");
-  // $(this).remove();
+  }
 
-  // flipCard();
-});
+  playButton.click(startTimer);
+  restartButton.click(restartGame);
+  cardStyle.click(selectCard);
+  blankCard.click(selectBlankCard);
+  initialGameState();
+  /* function flipCard () {
+
+  // actually shuffle and display cards upon flip, might want to leave it outside function to randomize right away
+// $(".fab").css("display", "block");
+// blankCard.remove();
+//   animateCard.addClass("animated flipInY slow delay-4s");
+//
+}*/
+
 
 });
